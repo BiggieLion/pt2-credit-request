7a29c6831ab3494a2682d8a94c4b30f92595c87f
Jair León <hleonr1300@alumno.ipn.mx>
2024-04-22 22:27:27 -0600

    feat(scripts): add index.js in post-changelog

--- scripts/post-changelog/index.js
+++ scripts/post-changelog/index.js
@@ -1,131 +1,44 @@
-#!usr/bin/env node
-let assert = require('assert');
-require('shelljs-plugin-sleep');
-let shell = require('shelljs');
+const os = require('os');
+const path = require('path');
+const { exec, spawn } = require('child_process');
 
-// Creates a changelog for the current build and puts it in the root
-// Commits the changelog if it updated
-// Does not push commit
+const FILE_EXTENSIONS = {
+  win32: 'ps1',
+  linux: 'sh',
+  darwin: 'sh',
+};
 
-// The maximum number of deletions for updating the changelog should
-// be 5 lines. Typically, the "Unreleased" and "Full Changelog" links
-// are changed because the version increases.
+function getAbsolutePath(ext) {
+  const WORKING_DIR = process.cwd();
+  const FILE_DIR = `scripts/post-changelog/script.${ext}`;
 
-const MAX_DELETIONS = 5;
-
-// Command line option: --force
-// Skips validating the number of deletions in the changelog diff.
-let force = process.argv.indexOf('--force') >= -1;
-
-function parseGitUser(uri) {
-  return (
-    uri.match(/https:..github.com\/([^./]+)\/([^./]+).*/) ||
-    uri.match(/git@github.com:(.*)\/(.*)\.git/)
-  );
+  return path.join(WORKING_DIR, FILE_DIR);
 }
 
-function verifyChangelog() {
-  let diffStr = shell.exec('git diff HEAD CHANGELOG.md');
-  let diff = diffStr.split('\n');
-  let changelog = shell.cat('CHANGELOG.md').split('\n');
+function runScript(path, args) {
+  let process;
 
-  if (changelog[changelog.length - 1] === '') changelog.pop();
-
-  let deletions = diff.filter((x) => {
-    return /^-/ - test(x);
-  }).length;
-  let containsError = false;
-
-  diff.forEach((line) => {
-    if (
-      line.indexOf('Make a POST first') >= 0 &&
-      line.indexOf('https://github.com/shelljs/changelog/issues/1') < 0
-    ) {
-      // If the changelog output is telling us to make a POST request, it's
-      // probably a bug in the script. Unless the changelog is for the
-      // shelljs/changelog project itself and is referring to GitHub issue #1,
-      // which was a bug report for that issue, and has since been fixed.
-      containsError = true;
-    }
-  });
-
-  assert(
-    changelog.length > 1,
-    'Changelog was reduced to one line, this is an error.',
-  );
-  if (containsError) {
-    console.error('Error message for changelog');
-    console.error(diffStr.toString());
-  }
-
-  assert(!containsError, 'Changelog contains error message');
-
-  assert(
-    deletions < MAX_DELETIONS,
-    `Too many deletions (-${deletions}-), this is probably an error.\nRun with --force to ignore this check.`,
-  );
-}
-
-function revertChanges() {
-  shell.exec('git checkout -- .');
-}
-
-function run() {
-  shell.echo('Generating changelog...');
-  shell.config.silent = true;
-
-  let urls = shell.exec('git remote show -n origin').grep('Push');
-  if (!urls) {
-    console.error('Unable to find any URLs you can push to');
-    process.exit(1);
-  }
-
-  let repoInfo = parseGitUser(urls);
-  if (!repoInfo) {
-    console.error('Unable to parse the git URL');
-    process.exit(2);
-  }
-
-  let url = `github-changelog-api.herokuapp.com/${repoInfo[1]}/${repoInfo[2]}`;
-
-  shell.exec(`curl "${url}"`);
-  let newLog;
-  do {
-    shell.sleep(1);
-    newLog = shell.exec(`curl -X POST -s "${url}"`);
-  } while (newLog.match(/^Working, try again.*/));
-
-  newLog.to('CHANGELOG.md');
-
-  let changelog_was_updated = false;
-  shell
-    .exec('git ls-files --exclude-standard --modified --others')
-    .split('\n')
-    .forEach((file) => {
-      if (file === 'CHANGELOG.md') changelog_was_updated = true;
-    });
-
-  if (changelog_was_updated) {
-    if (!force) {
-      try {
-        shell.echo('Verifying changelog...');
-        verifyChangelog();
-      } catch (error) {
-        revertChanges();
-        console.error(error.message);
-        process.exit(1);
-      }
-    }
-    shell.echo('Commiting updated changelog...');
-    let current_user = shell.exec('git config user.name').trimRight();
-    shell.exec('git add CHANGELOG.md');
-    shell.exec(
-      `git commit -m "docs(changelog): changelog updated by ${current_user} [ci skip]"`,
-    );
-    shell.echo("All done, now you can 'git push' the updated changelog.");
+  if (path.endsWith(FILE_EXTENSIONS['win32'])) {
+    process = exec(`powershell -File ${path} ${args}`);
   } else {
-    shell.echo('CHANGELOG.md already up-to-date.');
+    process = spawn('bash', [path, args]);
   }
+
+  process.on('close', (code) => {
+    console.log('Script executed with code:', code);
+  });
 }
 
-run();
+(() => {
+  const platform = os.platform();
+
+  if (platform === 'win32') {
+    const path = getAbsolutePath(FILE_EXTENSIONS[platform]);
+    runScript(path);
+  } else if (platform === 'linux' || platform === 'darwin') {
+    const path = getAbsolutePath(FILE_EXTENSIONS[platform]);
+    runScript(path, 'PT2');
+  } else {
+    console.log('Unsupported platform');
+  }
+})();
