1ee64bf56bef5d56c023a70b29f25d43af6694c6
Jair León <hleonr1300@alumno.ipn.mx>
2024-04-28 19:51:03 -0600

    build(project): add api module and delete app files, main.ts modified

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { configuration, validationSchema } from 'config';
 
@@ -12,7 +10,5 @@ import { configuration, validationSchema } from 'config';
       validationSchema,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class ApiModule {}
